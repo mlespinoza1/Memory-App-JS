8c96d7068af7495c64de934e2f8dd41004ea5744
mlespinoza1 <mr.espinoza78@gmail.com>
2024-09-26 23:18:03 -0600

    Update via Raycast script

---
+++ src/components/CreateNewOverlay.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface CreateNewOverlayProps {
+  // Add any props you need here
+}
+
+const CreateNewOverlay: React.FC<CreateNewOverlayProps> = () => {
+  return (
+    <div>
+      <h2>Create New Overlay</h2>
+      {/* Add your overlay content here */}
+    </div>
+  );
+};
+
+export default CreateNewOverlay;
